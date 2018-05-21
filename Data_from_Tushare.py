f6e13c1523d8a008f498d38d010cdd33257ef090
Mark Qian <35564473+qhq985@users.noreply.github.com>
2018-05-21 18:04:49 +0800

    I change exporting csv to a function

--- Data_from_Tushare.py
+++ Data_from_Tushare.py
@@ -5,9 +5,6 @@ import sys
 from bs4 import BeautifulSoup
 import ssl
 import tushare as ts
-import pandas as pd
-import numpy
-import csv
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -21,32 +18,36 @@ def urlTolist():
     s = r'<li><a target="_blank" href="http://quote.eastmoney.com/\S\S(.*?).html">'
     pat = re.compile(s)
     code = pat.findall(html)
-    #print(code)
     for item in code:
+        # 6 with 上海A股， 3 with 创业板， 0 with 深圳A股 总共3633只A股
         if item[0] == '6' or item[0] == '3' or item[0] == '0':
             allCodeList.append(item)
     return allCodeList
 
-
-
 def getHistoryData(code,time):
     '''Get the history of code and return the Day, Week, or Month line'''
     if time=='D':
-        return ts.get_hist_data(code, start = '2013-01-01')
+        return ts.get_hist_data(code)
     elif time=='W':
-        return ts.get_hist_data(code, start = '2013-01-01', ktype='W')
+        return ts.get_hist_data(code,ktype='W')
     elif time=='M':
-        return ts.get_hist_data(code, start = '2013-01-01', ktype='M')
+        return ts.get_hist_data(code,ktype='M')
 
-original_codes = urlTolist()
-period = ['M','W', 'D']
-for p in period:
-    for code in original_codes:
-        df = getHistoryData(code,p)
-        if p == 'M':
-            df.to_csv('./month/'+ code + '.csv')
-        if p == 'W':
-            df.to_csv('./week/'+ code + '.csv')
-        if p == 'D':
-            df.to_csv('./day/'+ code + '.csv')
-        print(code,p," is Done!")
+
+def download_data(period,totalcodes):
+    '''Download all history data of code to local'''
+    print('The total number of type A stock is',len(original_code),'\n')
+    for p in period:
+        for code in totalcodes:
+            df = getHistoryData(code,p)
+            if p == 'M':
+                df.to_csv('./month/'+ code + '.csv')
+            if p == 'W':
+                df.to_csv('./week/'+ code + '.csv')
+            if p == 'D':
+                df.to_csv('./day/'+ code + '.csv')
+            print(code,p," is Done!")
+
+original_code = urlTolist()
+# download_data(['M','W','D'],original_code)
+
